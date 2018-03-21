9d2c5e87026606bf6b602a8f5a7ceab9174970e0
duwj9283 <duwj9283@163.com>
2018-03-21 10:39:49 +0800

    最终初始化版本

--- src/main/java/com/wjdu/res/service/SpiderService.java
+++ src/main/java/com/wjdu/res/service/SpiderService.java
@@ -39,12 +39,8 @@ public class SpiderService{
 	//时事新闻
 	private final static String SsxwDomain = Domain+"xwzxdt/";
 
-	//agent
+	//Agent
 	private final static String agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36";
-
-//	public static void main(String[] args) {
-//
-//	}
 
 	public void getData(){
 		try {
@@ -53,48 +49,6 @@ public class SpiderService{
 			e.printStackTrace();
 		}
 	}
-
-//	private boolean isContainChinese(String str) {
-//		Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
-//		Matcher m = p.matcher(str);
-//		if (m.find()) {
-//			return true;
-//		}
-//		return false;
-//	}
-
-//	private String getRealUrl(String url,String fatherUrl){
-//		if(url.contains("../")){
-//			if(fatherUrl.contains(".html")){
-//				String[] strArr = fatherUrl.split("/");
-//				fatherUrl = fatherUrl.substring(0,fatherUrl.length()-strArr[strArr.length-1].length()-strArr[strArr.length-2].length()-1);
-//			}else{
-//				String[] strArr = fatherUrl.split("/");
-//				fatherUrl = fatherUrl.substring(0,fatherUrl.length()-strArr[strArr.length-1].length()-1);
-//			}
-//			return fatherUrl + url.replaceFirst("../","");
-//		}else if(url.contains("./")){
-//			if(fatherUrl.contains(".html")){
-//				String[] strArr = fatherUrl.split("/");
-//				fatherUrl = fatherUrl.substring(0,fatherUrl.length()-strArr[strArr.length-1].length());
-//			}
-//			return fatherUrl + url.replaceFirst("./","");
-//		}else if(url.contains("http")){
-//			return url;
-//		}else{
-//			return Domain + url.substring(1,url.length());
-//		}
-//	}
-
-//	private String getTypeUrl(String url){
-//		if(url.contains("../")){
-//			return SsxwDomain + url.replaceFirst("../","");
-//		}else if(url.contains("./")){
-//			return SsxwDomain + "jryw/" + url.replaceFirst("./","");
-//		}else{
-//			return Domain + url.substring(1,url.length());
-//		}
-//	}
 
 	private Document connect(String url) {
 		Document doc = null;
@@ -126,24 +80,21 @@ public class SpiderService{
 						logger.warn(">>>["+firstChild.text()+"]开始更新<<<");
 						String href = firstChild.attr("abs:href");
 						logger.warn("第[1]页:" + href);
-						if("http://www.hefei.gov.cn/xwzxdt/nrtj/".equals(href)){    //待删除
-                            getNewsList(href,from);
-                            doc = connect(href);
-                            if(doc.getElementsByClass("page").size()>0){
-                                String totalPage = doc.getElementsByClass("page").html().replaceAll("^<script>createPageHTML\\(([0-9]+),[\\s\\S]*$","$1");
-                                if(StringUtils.isNotEmpty(totalPage)){
-                                    Integer totalCount = Integer.parseInt(totalPage);
-                                    logger.warn("总页数:"+totalCount);
-                                    if(totalCount>1){
-                                        for (int k=1;k<totalCount;k++){
-                                            logger.warn("第["+(k+1)+"]页:"+href+"index_"+k+".html");
-                                            getNewsList(href+"index_"+k+".html",from);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
+						getNewsList(href,from);
+						doc = connect(href);
+						if(doc.getElementsByClass("page").size()>0){
+							String totalPage = doc.getElementsByClass("page").html().replaceAll("^<script>createPageHTML\\(([0-9]+),[\\s\\S]*$","$1");
+							if(StringUtils.isNotEmpty(totalPage)){
+								Integer totalCount = Integer.parseInt(totalPage);
+								logger.warn("总页数:"+totalCount);
+								if(totalCount>1){
+									for (int k=1;k<totalCount;k++){
+										logger.warn("第["+(k+1)+"]页:"+href+"index_"+k+".html");
+										getNewsList(href+"index_"+k+".html",from);
+									}
+								}
+							}
+						}
 						logger.warn("***["+firstChild.text()+"]更新结束***");
 					}
 				}
@@ -155,71 +106,73 @@ public class SpiderService{
 	public void getNewsList(String baseUrl, String from) throws IOException {
 		Document doc;
 		doc = connect(baseUrl);
-		List<Node> newChilds = doc.select("ul.con_list").first().childNodes();
-		List<News> newsList = new ArrayList<>();
-		for (Node child : newChilds) {
-			if (child instanceof Element) {
-				News news = new News();
-				Element echild = (Element) child;
-				Elements elements = echild.children();
-				if (null != elements && elements.size()>0) {
-					Element firstChild = elements.first();
-					if (firstChild.tagName().equals("a")){
-						String href = firstChild.attr("abs:href");
-						news.setHref(href);
-						//新闻内容地址
-						String newsUrl =news.getHref();
-						Document newsDoc = connect(newsUrl);
-						if(null != newsDoc && newsDoc.getElementsByClass("title").size() == 0 && newsDoc.getElementsByClass("copy").size() == 0){
-							continue;
-						}
-                        logger.warn("新闻内容URl:"+newsUrl);
-						//新闻标题
-						String title = newsDoc.getElementsByClass("title").first().select("p").first().text();
-						Element contentElement = newsDoc.getElementsByClass("news_con").first();
-						//解析图片，并多线程下载
-						Elements imgTags = contentElement.select("img[src]");
-						String content = contentElement.html();
-						if(null != imgTags && imgTags.size()>0){
-							List<FutureTask<ImageDTO>> taskList = new ArrayList<FutureTask<ImageDTO>>();
-							for (Element imgTag : imgTags) {
-								String absSrc = imgTag.attr("abs:src");
-								String src = imgTag.attr("src");
-								GetPictureThread getPictureThread = new GetPictureThread(src,absSrc,"G:\\images");
-								FutureTask<ImageDTO> task = new FutureTask<ImageDTO>(getPictureThread);
-								taskExecutor.execute(task);
-								taskList.add(task);
+		if(doc.select("ul.con_list").size() > 0){
+			List<Node> newChilds = doc.select("ul.con_list").first().childNodes();
+			List<News> newsList = new ArrayList<>();
+			for (Node child : newChilds) {
+				if (child instanceof Element) {
+					News news = new News();
+					Element echild = (Element) child;
+					Elements elements = echild.children();
+					if (null != elements && elements.size()>0) {
+						Element firstChild = elements.first();
+						if (firstChild.tagName().equals("a")){
+							String href = firstChild.attr("abs:href");
+							news.setHref(href);
+							//新闻内容地址
+							String newsUrl =news.getHref();
+							Document newsDoc = connect(newsUrl);
+							if(null != newsDoc && newsDoc.getElementsByClass("title").size() == 0 && newsDoc.getElementsByClass("copy").size() == 0){
+								continue;
 							}
-							for (FutureTask<ImageDTO> task : taskList) {
-								try {
-									ImageDTO imageDTO = task.get(30, TimeUnit.SECONDS);
-									if (null != imageDTO) {
-										content = content.replace(imageDTO.getRemoteShortSrc(),imageDTO.getLocalScr());
+							logger.warn("新闻内容URl:"+newsUrl);
+							//新闻标题
+							String title = newsDoc.getElementsByClass("title").first().select("p").first().text();
+							Element contentElement = newsDoc.getElementsByClass("news_con").first();
+							//解析图片，并多线程下载
+							Elements imgTags = contentElement.select("img[src]");
+							String content = contentElement.html();
+							if(null != imgTags && imgTags.size()>0){
+								List<FutureTask<ImageDTO>> taskList = new ArrayList<FutureTask<ImageDTO>>();
+								for (Element imgTag : imgTags) {
+									String absSrc = imgTag.attr("abs:src");
+									String src = imgTag.attr("src");
+									GetPictureThread getPictureThread = new GetPictureThread(src,absSrc,"G:\\images");
+									FutureTask<ImageDTO> task = new FutureTask<ImageDTO>(getPictureThread);
+									taskExecutor.execute(task);
+									taskList.add(task);
+								}
+								for (FutureTask<ImageDTO> task : taskList) {
+									try {
+										ImageDTO imageDTO = task.get(30, TimeUnit.SECONDS);
+										if (null != imageDTO) {
+											content = content.replace(imageDTO.getRemoteShortSrc(),imageDTO.getLocalScr());
+										}
+									} catch (Exception e) {
+										logger.error("task error", e);
 									}
-								} catch (Exception e) {
-									logger.error("task error", e);
 								}
 							}
+							String date =  newsDoc.getElementsByClass("time").text().replace("&nbsp","").trim().substring(0,16);
+							news.setTitle(title);
+							news.setContent(content);
+							news.setDate(date);
+							int tagCount = newsDoc.getElementsByClass("cur_site").select("a").size();
+							Element curSite;
+							if(tagCount>2){
+								curSite =  newsDoc.getElementsByClass("cur_site").select("a").get(2);
+							}else{
+								curSite =  newsDoc.getElementsByClass("cur_site").select("a").get(tagCount-1);
+							}
+							String typeName = curSite.attr("title");
+							news.setType_name(typeName);
+							news.setFrom(from);
+							newsList.add(news);
 						}
-						String date =  newsDoc.getElementsByClass("time").text().replace("&nbsp","").trim().substring(0,16);
-						news.setTitle(title);
-						news.setContent(content);
-						news.setDate(date);
-						int tagCount = newsDoc.getElementsByClass("cur_site").select("a").size();
-						Element curSite;
-						if(tagCount>2){
-							curSite =  newsDoc.getElementsByClass("cur_site").select("a").get(2);
-						}else{
-							curSite =  newsDoc.getElementsByClass("cur_site").select("a").get(tagCount-1);
-						}
-						String typeName = curSite.attr("title");
-						news.setType_name(typeName);
-						news.setFrom(from);
-						newsList.add(news);
 					}
 				}
 			}
+			newsService.batchInsertNews(newsList);
 		}
-        newsService.batchInsertNews(newsList);
 	}
 }
--- src/main/java/com/wjdu/res/service/thread/GetPictureThread.java
+++ src/main/java/com/wjdu/res/service/thread/GetPictureThread.java
@@ -27,28 +27,32 @@ public class GetPictureThread implements Callable<ImageDTO> {
     @Override
     public ImageDTO call() throws Exception {
         ImageDTO imageDTO = new ImageDTO();
-        URL url;
-        InputStream in;
-        FileOutputStream file;
-        String fileName = "";
         try {
             // 获取图片名
-            fileName = this.remoteSrc.substring(this.remoteSrc.lastIndexOf("/")).replace("/", "");
-            // 初始化url对象
-            url = new URL(this.remoteSrc);
-            URLConnection conn = url.openConnection();
-            // 初始化in对象，也就是获得url字节流
-            conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-            in = conn.getInputStream();
-            byte[] bs = new byte[1024];
-            int len;
-            OutputStream os = new FileOutputStream(this.filePath + "\\" + fileName);
-            // 开始读取
-            while ((len = in.read(bs)) != -1) {
-                os.write(bs, 0, len);
+            String fileName = this.remoteSrc.substring(this.remoteSrc.lastIndexOf("/")).replace("/", "");
+            if(fileName.contains(".jpg")||fileName.contains(".gif")||fileName.contains(".png")){
+                fileName = fileName;
+            }else{
+                fileName = fileName+".jpg";
             }
-            os.close();
-            in.close();
+            File file=new File(this.filePath + "\\" + fileName);
+            if(!file.exists()){
+                // 初始化url对象
+                URL url = new URL(this.remoteSrc);
+                URLConnection conn = url.openConnection();
+                // 初始化in对象，也就是获得url字节流
+                conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+                InputStream in = conn.getInputStream();
+                byte[] bs = new byte[1024];
+                int len;
+                OutputStream os = new FileOutputStream(this.filePath + "\\" + fileName);
+                // 开始读取
+                while ((len = in.read(bs)) != -1) {
+                    os.write(bs, 0, len);
+                }
+                os.close();
+                in.close();
+            }
             imageDTO.setRemoteShortSrc(this.remoteShortSrc);
             imageDTO.setRemoteSrc(this.remoteSrc);
             imageDTO.setLocalScr(this.filePath + "\\" + fileName);
